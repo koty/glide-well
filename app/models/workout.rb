56d0380979a9fabd1f606c1277fc084038ada530
kouichi nishizawa <kouichi.nishizawa+bitbucket@gmail.com>
2019-05-29 15:24:30 +0900

    一覧で距離を算出するよう修正

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -13,4 +13,9 @@ class Workout < ApplicationRecord
   belongs_to :user
   enum kind: { swimming: 'swimming', bicycle: 'bicycle' }
   has_many :workout_details, dependent: :destroy
+
+  def total_distance
+    # workout_details.map(&:distance).sum
+    workout_details.map{|x| x.distance}.sum
+  end
 end
